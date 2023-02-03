df56375c7235c7955feacecb71b5f0515cd6c494
apprenticeUniverse <apprenticeuniverse23@gmail.com>
2023-02-03 02:21:13 -0600

    fix url

--- src/API/baseURL.js
+++ src/API/baseURL.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "ec2-3-133-108-158.us-east-2.compute.amazonaws.com:8080",
-}
+});
 
 if (localStorage.getItem("SavedToken")) {
   instance.defaults.headers.common["Authorization"] =
